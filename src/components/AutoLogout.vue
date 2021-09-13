e3216a9611e8d03287daa275b427bb6b12ed0c1f
Levi <90584200+LeviBcodes@users.noreply.github.com>
2021-09-12 20:56:32 -0700

    Setup methods for timers and user input

--- src/components/AutoLogout.vue
+++ src/components/AutoLogout.vue
@@ -1,14 +1,41 @@
 <template>
   <div>
-    
+    Hello
   </div>
 </template>
 
 <script>
 export default {
   name: 'AutoLogout',
-  props: {
-    msg: String
+  data: function () {
+    return{
+      //events or iput to listen for from the user
+      events: ['click', 'mousemove', 'mousedown', 'scroll', 'keypress', 'load'],
+      warningTimer: null,
+      logoutTimer: null,
+    }
+  },
+  mounted() {
+    //listen for any events from the user
+    this.events.forEach((event) => {
+      window.addEventListener(event, this.resetTimer)
+    });
+    this.setTimers();
+  },
+  methods: {
+    setTimers: function() {
+      //this method will invoke the warningMessage method giving the user feedback, warning them they are about to be logged out if they are inactive
+      this.warningTimer = setTimeout(this.warningMessage, 4 * 1000)
+    },
+    warningMessage: function () {
+      //this is the warning message
+      alert('warning');
+    },
+    resetTimer: function () {
+      //if any input is detected the warningTimer method will be reset, keeping the user signed in
+      clearTimeout(this.warningTimer);
+      this.setTimers();
+    }
   }
 }
 </script>
